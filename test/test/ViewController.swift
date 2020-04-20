95a5fbf6ec938643d9b3e1060e5446429009716f
Aviral Agarwal <aviralagarwal@MacBook-Pro-4.local>
2020-04-20 14:00:45 -0400

    Added StarryNight ML Model and some code along with it

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     @IBOutlet weak var imageOutlet: UIImageView!
@@ -128,5 +129,64 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         imageOutlet.image = chosenImage
 //        print(info)
     }
+
+
+//ML STUFF BEGINS DOWN HERE
+    
+    func stylizePic(){
+        let model = StarryStyle();
+        let styleArray = try? MLMultiArray(shape: [1] as [NSNumber], dataType: .double)
+        styleArray?[0] = 1.0
+        
+        if let image = pixelBuffer(from: imageOutlet.image!) {
+            do {
+                let predictionOutput = try model.prediction(image: image, index: styleArray!)
+                        
+                let ciImage = CIImage(cvPixelBuffer: predictionOutput.stylizedImage)
+                let tempContext = CIContext(options: nil)
+                let tempImage = tempContext.createCGImage(ciImage, from: CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(predictionOutput.stylizedImage), height: CVPixelBufferGetHeight(predictionOutput.stylizedImage)))
+                imageOutlet.image = UIImage(cgImage: tempImage!)
+            } catch let error as NSError {
+                print("CoreML Model Error: \(error)")
+            }
+        }
+    }
+    
+    
+    
+    func pixelBuffer(from image: UIImage) -> CVPixelBuffer? {
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 256, height: 256), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 256, height: 256))
+        _ = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+     
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, 256, 256, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+           
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: 256, height: 256, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: 256)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        image.draw(in: CGRect(x: 0, y: 0, width: 256, height: 256))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+            
+        return pixelBuffer
+    }
+
+
+
+
 }
 
